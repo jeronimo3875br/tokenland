befee501f4f836df302688402e7f35ddbc26ef92
Gabriel Jeronimo <gschimidit998919812@gmail.com>
2022-04-10 13:58:27 -0300

    info: first commit and initial project and structure settings.

---
+++ src/views/home.view.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Teste</h1>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+
+    @Component
+    export default class HomeView extends Vue {}
+</script>
